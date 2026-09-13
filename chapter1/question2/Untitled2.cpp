868f897b340b50eb252c019574744e2990a43823
Nguyen Dung <nguyendung@gmail.com>
2026-09-13 14:13:11 +0700

    change

--- chapter1/question2/Untitled2.cpp
+++ chapter1/question2/Untitled2.cpp
@@ -1,40 +1,51 @@
-#include <bits/stdc++.h> 
+#include <bits/stdc++.h>
 
-using namespace std; 
-// ham xu ly xau ki tu 
-int String_process( string line ) {
-	string command; 
-	int k; 
-	stringstream ss( line ); 
-	ss >> command >> k; // su dung stringstream de tach hai token co chua space 
-	return k; 
-} 
-int main(){
-	int n; cin >> n; // so phan tu trong mang 
-	vector<int> v; 
-	// nhap cac phan tu vao vector 
-	for ( int i = 0; i < n ; i++) {
-		int x; cin >> x; 
-		v.push(x); 
-	}
-	sort(v.begin(), v.end()); // sort hàm 
-	while(1){
-		String s; 
-		getline(s, cin ); 
-		if ( s == "#") return; 
-		else {
-			int val = String_process(s); // gia tri can dem di tim theo yc de bai 
-			int ckc = 0; 
-			for ( int i = 0 ; i < n ; i++){
-				if( v[i] == val ) {
-					cout << v[i++]; 
-					ckc = 1; 
-					break; 
-				}
-			}
-			if ( ckc = 0) cout << -1 << endl ;  
-		} 
-	} 
+using namespace std;
+
+// Ham xu ly xau ky tu
+int String_process(string line) {
+    string command;
+    int k;
+
+    stringstream ss(line);
+    ss >> command >> k;
+
+    return k;
 }
 
+int main() {
+    int n;
+    cin >> n;
 
+    vector<int> v;
+
+    // Nhap cac phan tu vao vector
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        v.push_back(x);
+    }
+
+    sort(v.begin(), v.end());
+
+    cin.ignore();
+
+    while (1) {
+        string s;
+        getline(cin, s);
+
+        if (s == "#")
+            break;
+
+        int val = String_process(s);
+
+        auto it = upper_bound(v.begin(), v.end(), val);
+
+        if (it != v.end())
+            cout << *it << endl;
+        else
+            cout << -1 << endl;
+    }
+
+    return 0;
+}
